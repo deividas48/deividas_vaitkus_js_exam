3cf131be45ee329224f6f1a14320426ccbefea52
deividas48 <deividas.v6@gmail.com>
2023-12-10 00:42:57 +0200

    Add calculation header

--- Task 1/script.js
+++ Task 1/script.js
@@ -9,6 +9,7 @@ Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 div elemente su id "output" viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
 const form = document.querySelector("form");
+const inputSearch = document.getElementById('search');
 //Create button
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -25,9 +26,13 @@ form.addEventListener("submit", (event) => {
   while (outputEl.firstChild) {
     outputEl.removeChild(outputEl.firstChild);
   }
-
+  
+  const h2El = document.createElement("h2");
+  outputEl.append(h2El);
+  
   //Create the elements and fill in the data
   for (const unit in object) {
+    h2El.textContent = 'Calculation';
     const ulEl = document.createElement("ul");
     outputEl.append(ulEl);
     const liEl = document.createElement("li");
