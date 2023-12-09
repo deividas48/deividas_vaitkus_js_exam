de6a35e813779bd315601e3ecac8095fcf36a06d
deividas48 <deividas.v6@gmail.com>
2023-12-10 00:14:17 +0200

    Create elements and fill data

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,13 +8,28 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 div elemente su id "output" viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
-const form = document.querySelector('form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const inputSearchValue = inputSearch.value;
-    const object ={
-        lb: inputSearchValue * 2.2046,
-        g: inputSearchValue / 0.0010000,
-        oz: inputSearchValue * 35.274,
-    }
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputSearchValue = inputSearch.value;
+  const object = {
+    lb: inputSearchValue * 2.2046,
+    g: inputSearchValue / 0.001,
+    oz: inputSearchValue * 35.274,
+  };
+
+  const outputEl = document.getElementById("output");
+
+  //Norint kad būtų tik paskutinis skaičiavimas:
+  while (outputEl.firstChild) {
+    outputEl.removeChild(outputEl.firstChild);
+  }
+
+  for (const unit in object) {
+    const ulEl = document.createElement("ul");
+    outputEl.append(ulEl);
+    const liEl = document.createElement("li");
+    ulEl.append(liEl);
+    liEl.textContent = `${unit}: ${object[unit]}`;
+  }
 });
