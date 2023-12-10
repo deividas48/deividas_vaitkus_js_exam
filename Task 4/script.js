b743772ea966a222ea2da7009db3ca24cfbb5adf
deividas48 <deividas.v6@gmail.com>
2023-12-11 00:47:27 +0200

    Fetch and add cards with data

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,3 +9,35 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const fetchData = async () => {
+    try {
+      const response = await fetch(ENDPOINT);
+      if (response.ok) {
+        const data = await response.json();
+        renderCar(data);
+      } else {
+        console.error('Failed to fetch data');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  fetchData();
+
+  const outputDiv = document.getElementById("output");
+
+  renderCar = (cars) => {
+    cars.forEach(element => {
+        const card = document.createElement("figure");
+        outputDiv.append(card);
+        
+        const h2ElBrand = document.createElement("h2");
+        card.append(h2ElBrand);
+        h2ElBrand.textContent = element.brand;
+
+        const paragraphElModel = document.createElement("p");
+        card.append(paragraphElModel);
+        paragraphElModel.textContent = element.models;
+    });
+  };
