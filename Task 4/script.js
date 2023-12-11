26c7078d9a9c8bf20394ae3d21967f3d9844d339
deividas48 <deividas.v6@gmail.com>
2023-12-11 02:00:14 +0200

    Add style to cards

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,36 +8,48 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
 
+//Fetch
 const fetchData = async () => {
-    try {
-      const response = await fetch(ENDPOINT);
-      if (response.ok) {
-        const data = await response.json();
-        renderCar(data);
-      } else {
-        console.error('Failed to fetch data');
-      }
-    } catch (error) {
-      console.error(error);
+  try {
+    const response = await fetch(ENDPOINT);
+    if (response.ok) {
+      const data = await response.json();
+      renderCar(data);
+    } else {
+      console.error("Failed to fetch data");
     }
-  };
-  fetchData();
+  } catch (error) {
+    console.error(error);
+  }
+};
+fetchData();
 
-  const outputDiv = document.getElementById("output");
+const outputDiv = document.getElementById("output");
 
-  renderCar = (cars) => {
-    cars.forEach(element => {
-        const card = document.createElement("figure");
-        outputDiv.append(card);
-        
-        const h2ElBrand = document.createElement("h2");
-        card.append(h2ElBrand);
-        h2ElBrand.textContent = element.brand;
+//Create cards
+renderCar = (cars) => {
+  cars.forEach((element) => {
+    const card = document.createElement("figure");
+    outputDiv.append(card);
+    card.classList.add("carCard");
 
-        const paragraphElModel = document.createElement("p");
-        card.append(paragraphElModel);
-        paragraphElModel.textContent = element.models;
-    });
-  };
+    const h2ElBrand = document.createElement("h2");
+    card.append(h2ElBrand);
+    h2ElBrand.textContent = element.brand;
+    
+    const ulElModel = document.createElement("ul");
+    card.append(ulElModel);
+
+    //Create models li and adding commas
+    element.models.forEach((model, index) => {
+        const liElModel = document.createElement("li");
+        ulElModel.append(liElModel);
+        liElModel.textContent = model;
+        if (index < element.models.length - 1) {
+            liElModel.textContent += ",";
+        }
+      });
+  });
+};
