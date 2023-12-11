dfb580467018e741f73a74212c4f3802a90eee09
deividas48 <deividas.v6@gmail.com>
2023-12-11 10:45:37 +0200

    Create two functions

--- Task 6/script.js
+++ Task 6/script.js
@@ -9,13 +9,33 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+//Task1
+function getUserAverageAge() {
+  let avg;
+  let total = 0;
+  users.forEach((num) => {
+    total += num.age;
+  });
+  avg = total / users.length;
+  console.log("avg ===", avg);
+}
+getUserAverageAge();
+
+//Task2
+function getUsersNames(usersArray) {
+  const namesArray = usersArray.map(user => user.name);
+  return namesArray;
+};
+const names = getUsersNames(users);
+console.log(names);
